633d18a48052c4b42372059668eaedb31fc83d2f
srdjan031 <obrad.s031@gmail.com>
2019-02-25 00:51:10 +0100

    aaaaaaaaa

---
+++ script - Copyxxx.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+  $('.nav-button').click(function() {
+    $('.nav-button').toggleClass('change');
+  });
+
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if(position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+  });
+
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if(position >= 650) {
+      $('.camera-img').addClass('fromLeft');
+      $('.mission-text').addClass('fromRight');
+    } else {
+      $('.camera-img').removeClass('fromLeft');
+      $('.mission-text').removeClass('fromRight');
+    }
+  });
+
+  $('.gallery-list-item').click(function() {
+    let value = $(this).attr('data-filter');
+    if(value === 'all') {
+      $('.filter').show(300);
+    } else {
+      $('.filter').not('.' + value).hide(300);
+      $('.filter').filter('.' + value).show(300);
+    }
+  });
+
+  $('.gallery-list-item').click(function() {
+    $(this).addClass('active-item').siblings().removeClass('active-item');
+  });
+
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if(position >= 4300) {
+      $('.card-1').addClass('moveFromLeft');
+      $('.card-2').addClass('moveFromBottom');
+      $('.card-3').addClass('moveFromRight');
+    } else {
+      $('.card-1').removeClass('moveFromLeft');
+      $('.card-2').removeClass('moveFromBottom');
+      $('.card-3').removeClass('moveFromRight');
+    }
+  });
+});
+
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $(".navbar").addClass("d-none");
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $(".navbar").removeClass("d-none");
+  })
+
+})(jQuery); // End of use strict
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,121 @@
+$(document).ready(function () {
+  $('.nav-button').click(function () {
+    $('.nav-button').toggleClass('change');
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.nav-link').click(function () {
+    if ($(window).width() > 992) {
+      document.title = 'TITL';
+      $('.nav-button').removeClass('change');
+      $('.navbar-collapse').collapse('hide');
+    }
+    else {
+      document.title = 'blah';
+      $('.nav-button').toggleClass('change');
+      $('.navbar-collapse').collapse('hide');
+    }
+    // $('.nav-button').toggleClass('change');
+    // $('.navbar-collapse').collapse('hide');
+  });
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+  });
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 650) {
+      $('.camera-img').addClass('fromLeft');
+      $('.mission-text').addClass('fromRight');
+    } else {
+      $('.camera-img').removeClass('fromLeft');
+      $('.mission-text').removeClass('fromRight');
+    }
+  });
+
+  $('.gallery-list-item').click(function () {
+    let value = $(this).attr('data-filter');
+    if (value === 'all') {
+      $('.filter').show(300);
+    } else {
+      $('.filter').not('.' + value).hide(300);
+      $('.filter').filter('.' + value).show(300);
+    }
+  });
+
+  $('.gallery-list-item').click(function () {
+    $(this).addClass('active-item').siblings().removeClass('active-item');
+  });
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 4300) {
+      $('.card-1').addClass('moveFromLeft');
+      $('.card-2').addClass('moveFromBottom');
+      $('.card-3').addClass('moveFromRight');
+    } else {
+      $('.card-1').removeClass('moveFromLeft');
+      $('.card-2').removeClass('moveFromBottom');
+      $('.card-3').removeClass('moveFromRight');
+    }
+  });
+
+
+  // $( window ).resize(function() {
+  //   if ($( window ).width() > 992) {
+  //     $('.nav-button').removeClass('change');
+  //     $('.navbar-collapse').collapse('hide');}
+  // });
+
+  // Smooth scrolling using jQuery easing
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
